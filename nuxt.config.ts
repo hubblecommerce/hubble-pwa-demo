2efb3dd5053c68f0d5934309832450d4c006be41
Lukas Heinze <heinze@digitalmanufaktur.com>
2023-05-03 11:14:33 +0200

    Updated deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,20 +3,8 @@ export default defineNuxtConfig({
     modules: [
         '@hubblecommerce/hubble'
     ],
-    experimental: {
-        // https://github.com/nuxt/framework/issues/7517
-        treeshakeClientOnly: false,
-        // If components: global = true => all styles would be used inline. To avoid this use:
-        inlineSSRStyles: false
-    },
     nitro: {
         // https://github.com/unjs/nitro/pull/449
         compressPublicAssets: true
-    },
-    components: {
-        // Register all your components globally, which will create async chunks for all your components
-        // and prevent global prefetching
-        global: true,
-        dirs: ['~/components']
-    },
+    }
 })
